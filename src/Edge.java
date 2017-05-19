82347f5c513c0b50411c621a9c4fb551e0167590
samiabout <sami-about@hotmail.fr>
2017-05-19 16:17:29 +0200

    commit null

--- src/Edge.java
+++ src/Edge.java
@@ -1,7 +1,10 @@
-package PACKAGE_NAME;
-
 /**
  * Created by sami- on 19/05/2017.
  */
-public class Edge {
+public class Edge
+{
+    public void test({
+        System.out.println("test test");
+        System.out.println("heheh");
+    })
 }
